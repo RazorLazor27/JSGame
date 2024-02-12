3caa206c5187d520b6abb808c6b37dcb09019a7b
Nicolas Berenguela <nicolas.berenguela@gmail.com>
2024-02-12 01:10:30 -0300

    Camara 3ra persona lista

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 import { addStar, addSphere } from './src/geometry.js';
 
 const MAX_STAR = 200; 
+const width = window.innerWidth;
+const height = window.innerHeight;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+// const camera = new THREE.OrthographicCamera( width / - 24, width / 24, height / 24, height / - 24, 1, 1000 );
+scene.add( camera );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -38,6 +43,16 @@ const background = [];
 const sphere = addSphere();
 background.push(sphere);
 
+
+const cameraDistance = 12; // Distance from sphere
+const cameraOffset = new THREE.Vector3(0, 10, cameraDistance); // Offset from sphere
+const cameraTarget = new THREE.Vector3(); // Camera target (sphere position)
+const cameraLookAt = new THREE.Vector3(0, 0, 0); // Look at origin
+camera.position.copy(cameraOffset); // Set initial camera position
+camera.lookAt(cameraLookAt); // Look at origin
+
+
+
 for (let i = 0; i < MAX_STAR; i++){
   const star = addStar();
   background.push(star);
@@ -49,7 +64,7 @@ background.forEach( object => { scene.add( object ); })
 
 
 
-camera.position.z = 30;
+
 
 
 function animate() {
@@ -58,6 +73,11 @@ function animate() {
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.01;
+
+  cameraTarget.copy(sphere.position);
+  cameraTarget.add(cameraOffset);
+  camera.position.lerp(cameraTarget, 0.1); // Smoothly interpolate camera position
+  camera.lookAt(sphere.position); // Look at sphere
   controls.update;
 
 	renderer.render( scene, camera );
